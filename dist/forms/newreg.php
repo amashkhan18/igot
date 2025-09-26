94c15df607c873e7ce2237eed28da90fcb73db9e
amash <amashkhan18@gmail.com>
2025-09-26 15:11:13 +0530

    edited files from server and ignored database.php

--- dist/forms/newreg.php
+++ dist/forms/newreg.php
@@ -48,7 +48,7 @@ $conn->close();
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>New Karmyogi Registration</title>
+    <title>New Karmyogi Registrations</title>
     <link
       rel="stylesheet"
       href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css"
